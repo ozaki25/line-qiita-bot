d4a07c6f7f56d4b8ee4ee78003e125e3edd0ec52
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-25 14:30:32 +0900

    get message

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,11 +1,35 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
 
+type MessageType = {
+  events: {
+    type: string;
+    replyToken: string;
+    source: {
+      userId: string;
+      type: string;
+    };
+    timestamp: number;
+    mode: string;
+    message: {
+      type: string;
+      id: string;
+      text: string;
+    };
+  }[];
+  destination: string;
+};
+
 export const hello: APIGatewayProxyHandler = async event => {
-  const body = JSON.parse(event.body);
-  console.log({ body });
+  const body: MessageType = JSON.parse(event.body);
+  console.dir({ body });
+  const { events, destination } = body;
+  console.dir({ events, destination });
+  events.map(event => console.dir(event.message));
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: body.message }),
+    body: JSON.stringify({
+      message: body.events.map(event => event.message.text).join(', '),
+    }),
   };
 };
