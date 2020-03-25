50c2ed3a51c5828025b165a7acd3bd5e1a79e15e
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-25 15:01:49 +0900

    オウム返しまで

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,5 +1,10 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import 'source-map-support/register';
+import axios from 'axios';
+
+const lineApiURL = 'https://api.line.me/v2/bot/message';
+
+const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
 
 type MessageType = {
   events: {
@@ -20,16 +25,35 @@ type MessageType = {
   destination: string;
 };
 
+function reply({ text, replyToken }) {
+  const headers = {
+    'content-type': 'application/json',
+    Authorization: `Bearer ${channelAccessToken}`,
+  };
+  return axios.post(
+    `${lineApiURL}/reply`,
+    { replyToken, messages: [{ type: 'text', text }] },
+    { headers },
+  );
+}
+
 export const hello: APIGatewayProxyHandler = async event => {
   const body: MessageType = JSON.parse(event.body);
-  console.dir({ body });
-  const { events, destination } = body;
-  console.dir({ events, destination });
-  events.map(event => console.dir(event.message));
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: body.events.map(event => event.message.text).join(', '),
-    }),
-  };
+
+  try {
+    const result = await Promise.all(
+      body.events.map(async event => {
+        console.log(JSON.stringify(event));
+        return reply({
+          replyToken: event.replyToken,
+          text: event.message.text,
+        });
+      }),
+    );
+    console.log({ result });
+    return { statusCode: 200, body: 'OK' };
+  } catch (error) {
+    console.log(error.message);
+    return { statusCode: 500, body: 'NG' };
+  }
 };
