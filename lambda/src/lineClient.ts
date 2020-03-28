e085cf6756bc0b3966dac0a55470959b30065655
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-28 21:47:22 +0900

    add rich menu

--- lambda/src/lineClient.ts
+++ lambda/src/lineClient.ts
@@ -1,4 +1,4 @@
-import { Client } from '@line/bot-sdk';
+import { Client, RichMenu } from '@line/bot-sdk';
 
 const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
 
@@ -10,4 +10,43 @@ export function reply({ text, replyToken }) {
 
 export function push({ userId, text }) {
   return lineClient.pushMessage(userId, [{ type: 'text', text }]);
+}
+
+export function createRichmenu() {
+  const richmenu: RichMenu = {
+    size: {
+      width: 2500,
+      height: 1686,
+    },
+    selected: false,
+    name: 'LINE Developers Info',
+    chatBarText: 'Tap to open',
+    areas: [
+      {
+        bounds: {
+          x: 34,
+          y: 24,
+          width: 169,
+          height: 193,
+        },
+        action: {
+          type: 'uri',
+          uri: 'https://developers.line.biz/en/news/',
+        },
+      },
+      {
+        bounds: {
+          x: 229,
+          y: 24,
+          width: 207,
+          height: 193,
+        },
+        action: {
+          type: 'uri',
+          uri: 'https://www.line-community.me/',
+        },
+      },
+    ],
+  };
+  return lineClient.createRichMenu(richmenu);
 }
