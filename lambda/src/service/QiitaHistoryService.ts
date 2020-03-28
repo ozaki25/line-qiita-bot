8d7b4a7ddf9eafee76f71895985c1cbdc61699ea
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-29 03:08:16 +0900

    refactor QiitaHistoryService

--- lambda/src/service/QiitaHistoryService.ts
+++ lambda/src/service/QiitaHistoryService.ts
@@ -1,32 +1,29 @@
 import { DynamoDB } from 'aws-sdk';
-import { DocumentClient } from 'aws-sdk/clients/dynamodb';
 
 const tableName = process.env.QIITA_HISTORY_TABLE;
 
-class QiitaHistoryService {
-  private dynamo: DocumentClient;
+const dynamo = new DynamoDB.DocumentClient();
 
-  constructor() {
-    this.dynamo = new DynamoDB.DocumentClient();
-  }
-
-  findByUserId({ userId }) {
-    const params = { TableName: tableName, Key: { userId } };
-    return this.dynamo.query(params).promise();
-  }
-
-  findByUserIdAndDate({ userId, date }) {
-    const params = { TableName: tableName, Key: { userId, date } };
-    return this.dynamo.get(params).promise();
-  }
-
-  put({ userId, date, items, total }) {
-    const params = {
-      TableName: tableName,
-      Item: { userId, date, items, total },
-    };
-    return this.dynamo.put(params).promise();
-  }
+function findByUserId({ userId }) {
+  const params = { TableName: tableName, Key: { userId } };
+  return dynamo.query(params).promise();
 }
 
-export const userService = new QiitaHistoryService();
+function findByUserIdAndDate({ userId, date }) {
+  const params = { TableName: tableName, Key: { userId, date } };
+  return dynamo.get(params).promise();
+}
+
+function put({ userId, date, items, total }) {
+  const params = {
+    TableName: tableName,
+    Item: { userId, date, items, total },
+  };
+  return dynamo.put(params).promise();
+}
+
+export const qiitaHistoryService = {
+  findByUserId,
+  findByUserIdAndDate,
+  put,
+};
