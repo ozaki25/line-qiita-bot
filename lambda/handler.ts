940ed7a657013a12edee1b38d805115637e871a4
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-29 03:54:35 +0900

    push daily like count endpoint

--- lambda/handler.ts
+++ lambda/handler.ts
@@ -1,5 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { DocumentClient } from 'aws-sdk/clients/dynamodb';
+import * as dayjs from 'dayjs';
 import 'source-map-support/register';
 import { reply, push } from './src/lineClient';
 import { userService } from './src/service/UserService';
@@ -64,14 +65,36 @@ export const addUser: APIGatewayProxyHandler = async e => {
 
 export const saveQiitaInfo: APIGatewayProxyHandler = async () => {
   try {
-    const result: DocumentClient.ScanOutput = await userService.scan();
-    console.log(JSON.stringify(result));
+    const users: DocumentClient.ScanOutput = await userService.scan();
+    console.log(JSON.stringify(users));
 
-    const qiitaIds = uniq(result.Items.map(({ qiitaId }) => qiitaId));
+    const qiitaIds = uniq(users.Items.map(({ qiitaId }) => qiitaId));
     console.log({ qiitaIds });
 
     await Promise.all(
       qiitaIds.map(userId => qiitaService.saveItemInfo({ userId })),
+    );
+
+    return { statusCode: 200, body: 'OK' };
+  } catch (error) {
+    console.log(error.message);
+    return { statusCode: 500, body: 'NG' };
+  }
+};
+
+export const pushDailyLikeCount: APIGatewayProxyHandler = async () => {
+  try {
+    const startDate = dayjs()
+      .subtract(1, 'day')
+      .format('YYYY-MM-DD');
+    const endDate = dayjs().format('YYYY-MM-DD');
+    const users: DocumentClient.ScanOutput = await userService.scan();
+    console.log(JSON.stringify(users));
+
+    await Promise.all(
+      users.Items.map(({ lineId, qiitaId }) =>
+        qiitaService.pushLikeCount({ lineId, qiitaId, startDate, endDate }),
+      ),
     );
 
     return { statusCode: 200, body: 'OK' };
--- lambda/src/service/QiitaService.ts
+++ lambda/src/service/QiitaService.ts
@@ -1,6 +1,7 @@
 import * as dayjs from 'dayjs';
-import { qiitaApi } from '../api/Qiita';
+import { qiitaApi } from '../api/QiitaApi';
 import { qiitaHistoryService } from './QiitaHistoryService';
+import { push } from '../lineClient';
 
 async function getItems({ userId }) {
   try {
@@ -26,7 +27,33 @@ async function saveItemInfo({ userId }) {
   await qiitaHistoryService.put({ userId, date, items, total });
 }
 
+async function pushLikeCount({ lineId, qiitaId, startDate, endDate }) {
+  try {
+    const startDateHistory = await qiitaHistoryService.findByUserIdAndDate({
+      userId: qiitaId,
+      date: startDate,
+    });
+    const endDateHistory = await qiitaHistoryService.findByUserIdAndDate({
+      userId: qiitaId,
+      date: endDate,
+    });
+
+    if (!startDateHistory.Item || !endDateHistory.Item) return null;
+
+    const count = endDateHistory.Item.total - startDateHistory.Item.total;
+
+    await push({
+      userId: lineId,
+      text: `${endDate}のいいね数は${count}件でした！`,
+    });
+  } catch (e) {
+    console.log(e);
+    return null;
+  }
+}
+
 export const qiitaService = {
   getItems,
   saveItemInfo,
+  pushLikeCount,
 };
