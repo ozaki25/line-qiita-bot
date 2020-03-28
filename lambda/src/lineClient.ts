1629ad010e20df79009664b60417abf711bfc0f4
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-28 23:24:01 +0900

    refactor

--- lambda/src/lineClient.ts
+++ lambda/src/lineClient.ts
@@ -1,6 +1,6 @@
 import { Client } from '@line/bot-sdk';
 
-const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN;
+const channelAccessToken = process.env.CHANNEL_ACCESS_TOKEN || '';
 
 const lineClient = new Client({ channelAccessToken });
 
