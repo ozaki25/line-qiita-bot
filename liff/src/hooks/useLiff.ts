106510aadd5b9907c51f7174e791f7a2b172e692
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-29 16:23:22 +0900

    add useLiff

---
+++ liff/src/hooks/useLiff.ts
@@ -0,0 +1,56 @@
+import { useState, useEffect } from 'react';
+import { liff } from '../lib/liff';
+
+type Props = {
+  liffId: string;
+};
+
+function useLiff({ liffId }: Props) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  const initLiff = async ({ liffId }: { liffId: string }) => {
+    setLoading(true);
+    try {
+      await liff.init({ liffId });
+      console.log('success liff init');
+      if (liff.isLoggedIn()) {
+        console.log('logged in!');
+        await fetchProfile();
+      } else {
+        console.log('not logged in');
+        liff.login();
+      }
+    } catch (error) {
+      console.log({ error });
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const fetchProfile = async () => {
+    setLoading(true);
+    try {
+      setProfile(await liff.getProfile());
+    } catch (error) {
+      console.log({ error });
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (liffId) initLiff({ liffId });
+  }, [liffId]);
+
+  return {
+    loading,
+    error,
+    profile,
+  };
+}
+
+export default useLiff;
