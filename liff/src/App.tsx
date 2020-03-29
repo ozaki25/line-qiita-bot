ca5090da7a0045ab7af0c6f10c21a9f7fe2270bf
ozaki <fs65ugumori@yahoo.co.jp>
2020-03-29 16:16:15 +0900

    apply chakra

--- liff/src/App.tsx
+++ liff/src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { ThemeProvider } from '@chakra-ui/core';
 
 function App() {
   return (
-    <div>
+    <ThemeProvider>
       <h1>Hello LIFF</h1>
-    </div>
+    </ThemeProvider>
   );
 }
 
